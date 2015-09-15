2edeb93da80f1dd80085ceb2ddead5929939a329
mrmoor <mrmoor4@web.de>
2015-09-15 23:41:31 +0200

    comfortable config added

--- README.md
+++ README.md
@@ -22,12 +22,25 @@ npm install --save node-voronoi-map
 
 ```js
 var MapGenerator = require('node-voronoi-map');
-var mapGenerator = new MapGenerator();
+var mapGenerator = new MapGenerator({
+    width:1920,
+    height:960,
+    seed:992878989,
+    shapeSeed:60441019,
+    islandShape:'perlin',
+    pointSelection:'square',
+    oceanRatio:0.5,
+    islandFactor:1.07, //1.0 means no small islands; 2.0 leads to a lot
+    numberOfLands:'',
+    numberOfPoints:200,
+    lakeThreshold:0.3,
+    riverChance:100,
+    roadElevationThresholds:'0, 0.05, 0.37, 0.64',//comma separated
+});
 var map = mapGenerator.generate();
 ```
 
 Tasks
 -----
 
-* make comfortable config.
 * implement generate Map from Image
--- src/generator.js
+++ src/generator.js
@@ -3,8 +3,6 @@
     bitwise: true, camelcase: false, curly: true, eqeqeq: true, es3: true, evil: true, expr: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: single, regexdash: true, strict: true, sub: true, trailing: true, undef: true, unused: vars, white: true
 */
 
-'use strict';
-
 var _ = require('lodash');
 var islandShape = require('./island-shape');
 var lava = require('./lava');
@@ -15,7 +13,10 @@ var pointSelector = require('./point-selector');
 var roads = require('./roads');
 var watersheds = require('./watersheds');
 
-var config ={
+/**
+ * create Generator
+ * config param
+ * {
     width:1920,
     height:960,
     seed:992878989,
@@ -25,24 +26,30 @@ var config ={
     oceanRatio:0.5,
     islandFactor:1.07, //1.0 means no small islands; 2.0 leads to a lot
     numberOfLands:'',
-    numberOfPoints:20000,
+    numberOfPoints:200,
     lakeThreshold:0.3,
     riverChance:100,
     roadElevationThresholds:'0, 0.05, 0.37, 0.64',//comma separated
     edgeNoise:0.5,
     lloydIterations:2,
     imageThreshold:1.07,
-    invertImage:false,
 }
-
-function Generator(){
-
+ * @param config
+ * @constructor
+ */
+function Generator(config){
+    this.config = config;
 }
-Generator.prototype.constructor=Generator();
+Generator.prototype.constructor=Generator;
+
+/**
+ * get screen size
+ * @returns {{width: *, height: *}}
+ */
 Generator.prototype.getSize=function() {
     return {
-        width: config.width,
-        height: config.height
+        width: this.config.width,
+        height: this.config.height
     };
 }
 
@@ -60,6 +67,11 @@ Generator.prototype.getSize=function() {
     $(S_imageThumb).attr('src', imageDataUrl);
 }*/
 
+/**
+ *
+ * @param s
+ * @returns {*}
+ */
 Generator.prototype.getIntegerOrStringSeed=function(s) {
     if (_(s).isNumber()) {
         return _(s).parseInt();
@@ -68,6 +80,10 @@ Generator.prototype.getIntegerOrStringSeed=function(s) {
     return Math.abs(prng.stringToSeed(s));
 }
 
+/**
+ * generate map
+ * @returns {{map: null, noisyEdges: null, roads: null, watersheds: null, lava: null}}
+ */
 Generator.prototype.generate=function() {
     console.time('generate');
 
@@ -77,10 +93,10 @@ Generator.prototype.generate=function() {
     
     state.map = map({ width: size.width + 0.0, height: size.height + 0.0 });
 
-    var seed = this.getIntegerOrStringSeed(config.seed);
-    var shapeSeed = this.getIntegerOrStringSeed(config.shapeSeed);
+    var seed = this.getIntegerOrStringSeed(this.config.seed);
+    var shapeSeed = this.getIntegerOrStringSeed(this.config.shapeSeed);
     
-    switch (config.islandShape) {
+    switch (this.config.islandShape) {
     /*case 'bitmap' :
         var imageData = canvasCore.getImageData(image);
         var bitmap = canvasCore.makeAverageThresholdBitmap(imageData, config.imageThreshold);
@@ -96,10 +112,10 @@ Generator.prototype.generate=function() {
         state.map.newIsland(islandShape.makeNoise(shapeSeed), seed);
         break;
     case 'perlin' :
-        state.map.newIsland(islandShape.makePerlin(shapeSeed, config.oceanRatio), seed);
+        state.map.newIsland(islandShape.makePerlin(shapeSeed, this.config.oceanRatio), seed);
         break;
     case 'radial' :
-        state.map.newIsland(islandShape.makeRadial(shapeSeed, config.islandFactor), seed);
+        state.map.newIsland(islandShape.makeRadial(shapeSeed, this.config.islandFactor), seed);
         break;
     case 'square' :
         state.map.newIsland(islandShape.makeSquare(), seed);
@@ -113,27 +129,27 @@ Generator.prototype.generate=function() {
 
     var ps = (function (pointSelection, width, height, seed) { switch (pointSelection) {
         case 'random': return pointSelector.generateRandom(width, height, seed);
-        case 'relaxed': return pointSelector.generateRelaxed(width, height, seed, config.lloydIterations);
+        case 'relaxed': return pointSelector.generateRelaxed(width, height, seed, this.config.lloydIterations);
         case 'square': return pointSelector.generateSquare(width, height);
         case 'hex': return pointSelector.generateHexagon(width, height);
         default: throw 'unknown point selector ' + pointSelection;
-    }})(config.pointSelection, state.map.SIZE.width, state.map.SIZE.height, state.map.mapRandom.seed);
+    }})(this.config.pointSelection, state.map.SIZE.width, state.map.SIZE.height, state.map.mapRandom.seed);
 
-    var numberOfLands = config.numberOfLands;
+    var numberOfLands = this.config.numberOfLands;
     if (numberOfLands.length > 0) {
         mapLands.tryMutateMapPointsToGetNumberLands(state.map, ps, parseInt(numberOfLands, 10));
     } else {
-        state.map.go0PlacePoints(config.numberOfPoints, ps);
+        state.map.go0PlacePoints(this.config.numberOfPoints, ps);
         state.map.go1BuildGraph();
-        state.map.go2AssignElevations(config.lakeThreshold);
+        state.map.go2AssignElevations(this.config.lakeThreshold);
     }
-    state.map.go3AssignMoisture(config.riverChance);
+    state.map.go3AssignMoisture(this.config.riverChance);
     state.map.go4DecorateMap();
     
-    var thresholds = config.roadElevationThresholds;
+    var thresholds = this.config.roadElevationThresholds;
     state.roads.createRoads(state.map, thresholds);
     state.watersheds.createWatersheds(state.map);
-    state.noisyEdges.buildNoisyEdges(state.map, state.lava, seed, config.edgeNoise);
+    state.noisyEdges.buildNoisyEdges(state.map, state.lava, seed, this.config.edgeNoise);
 
     console.timeEnd('generate');
     return state;
